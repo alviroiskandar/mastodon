89b4ff1f9be09ea5a8e08d87f945be63dbcf5614
Eugen Rochko <eugen@zeonfederated.com>
2023-07-09 18:40:30 +0200

    Change markers API to use a replica

--- app/controllers/api/v1/markers_controller.rb
+++ app/controllers/api/v1/markers_controller.rb
@@ -7,7 +7,10 @@ class Api::V1::MarkersController < Api::BaseController
   before_action :require_user!
 
   def index
-    @markers = current_user.markers.where(timeline: Array(params[:timeline])).index_by(&:timeline)
+    ApplicationRecord.connected_to(role: :read, prevent_writes: true) do
+      @markers = current_user.markers.where(timeline: Array(params[:timeline])).index_by(&:timeline)
+    end
+
     render json: serialize_map(@markers)
   end
 
